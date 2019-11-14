6152d785462e40bb03cba2238593dce0d0749602
PaBLyS <pavlobokhan@gmail.com>
2019-11-14 13:32:31 +0200

    create customFooter component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,26 @@
   <div>
     <custom-header/>
     <nuxt/>
+    <custom-footer/>
   </div>
 </template>
 
 <script>
     import customHeader from "../components/customHeader";
+    import customFooter from "../components/customFooter";
 
     export default {
-        components: {customHeader}
+        components: {customHeader, customFooter}
     }
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Muli:300,400,700&display=swap');
+
   body {
-    font-family: 'muli', sans-serif;
+    font-family: 'Muli', sans-serif;
+    margin: 0;
+    padding: 0;
   }
+
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   mode: 'spa',
   /*
@@ -7,39 +6,34 @@ module.exports = {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'fonts', href: 'https://fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap&subset=latin-ext'}
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
   /*
   ** Nuxt.js dev-modules
   */
-  buildModules: [
-  ],
+  buildModules: [],
   /*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
+  modules: [],
   /*
   ** Build configuration
   */
@@ -47,7 +41,7 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
