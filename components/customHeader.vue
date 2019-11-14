bdde5e4664e4457eb114634193163db61771ff81
PaBLyS <pavlobokhan@gmail.com>
2019-11-14 13:48:00 +0200

    create politics page

--- components/customHeader.vue
+++ components/customHeader.vue
@@ -1,26 +1,26 @@
 <template>
   <section class="header">
     <div class="header__left">
-      Mini Blog
+      <NuxtLink class="link" to="/">Mini Blog</NuxtLink>
     </div>
     <ul class="header__right">
       <li class="header__right-link">
-        Home
+        <NuxtLink class="link" to="/">Home</NuxtLink>
       </li>
       <li class="header__right-link">
-        Politics
+        <NuxtLink class="link" to="/politics">Politics</NuxtLink>
       </li>
       <li class="header__right-link">
-        Tech
+        <NuxtLink class="link" to="/politics">Tech</NuxtLink>
       </li>
       <li class="header__right-link">
-        Entertainment
+        <NuxtLink class="link" to="/politics">Entertainment</NuxtLink>
       </li>
       <li class="header__right-link">
-        Travel
+        <NuxtLink class="link" to="/politics">Travel</NuxtLink>
       </li>
       <li class="header__right-link">
-        Sports
+        <NuxtLink class="link" to="/politics">Sports</NuxtLink>
       </li>
     </ul>
   </section>
@@ -37,9 +37,21 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 0 10px;
 
     &__left {
       font-size: 24px;
+
+      .link {
+        color: #000;
+        text-decoration: none;
+        transition: color .2s ease-in;
+
+        &:hover {
+          color: #2f89fc;
+        }
+
+      }
     }
 
     &__right {
@@ -49,15 +61,19 @@
       &-link {
         padding: 10px;
         margin: 0;
-        color: #000;
-        font-weight: 700;
-        text-decoration: none;
-        transition: color .2s ease-in;
         list-style: none;
         cursor: pointer;
 
-        &:hover {
-          color: #2f89fc;
+        .link {
+          color: #000;
+          font-weight: 700;
+          text-decoration: none;
+          transition: color .2s ease-in;
+
+          &:hover {
+            color: #2f89fc;
+          }
+
         }
       }
     }
---
+++ pages/politics.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="section">
+    <div class="section__grey">
+      <div class="wrap-1140">
+        <h4 class="category">Category</h4>
+        <h3 class="label">Sports</h3>
+        <p class="content">Category description here.. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error eius quo,
+          officiis non maxime quos reiciendis perferendis doloremque maiores!</p>
+      </div>
+    </div>
+    <div class="wrap-1140">
+      <pagination-list />
+    </div>
+  </section>
+</template>
+
+<script>
+  import paginationList from "../components/paginationList";
+
+    export default {
+        name: "politics",
+        components: {
+            paginationList
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .section {
+    padding-bottom: 80px;
+
+    &__grey {
+      background-color: #f8f9fa;
+      padding: 40px 0;
+    }
+
+    .wrap-1140 {
+      width: 1140px;
+      margin: 0 auto;
+
+      &:nth-child(2) {
+        padding-top: 80px;
+      }
+
+      .category {
+        line-height: 1.7;
+        color: gray;
+        font-weight: 300;
+        font-size: 16px;
+      }
+
+      .label {
+        line-height: 1.5;
+        color: #000;
+        font-size: 28px;
+        font-weight: 500;
+        margin: 0;
+      }
+
+      .content {
+        width: 50%;
+        line-height: 1.7;
+        color: gray;
+        font-weight: 300;
+        font-size: 16px;
+      }
+
+    }
+  }
+</style>
